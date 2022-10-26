24883bb6228f170226cfef0f2fd98bebb6693ebb
Miriam Smith <miriamzeff@gmail.com>
2022-10-26 15:24:11 +0300

    added meme editor section

--- js/controllers/memeController.js
+++ js/controllers/memeController.js
@@ -1,1 +1,39 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.getElementById('my-canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    // resizeCanvas()
+    // window.addEventListener('resize', resizeCanvas)
+
+    renderMeme()
+}
+
+function renderMeme() {
+
+    drawText()
+    drawImg()
+}
+
+function drawImg() {
+    const img = new Image() // Create a new html img element
+    img.src = 'img/gallery/1.jpg' // Send a network req to get that image, define the img src
+    // When the image ready draw it on the canvas
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    }
+}
+
+function drawText(text = 'Funny Stuff', x=40, y=70) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+
+    gCtx.font = '40px Impact'
+    gCtx.fillText(text, x, y) // Draws (fills) a given text at the given (x, y) position.
+    gCtx.strokeText(text, x, y) // Draws (strokes) a given text at the given (x, y) position.
+}
--- js/services/memeService.js
+++ js/services/memeService.js
@@ -2,7 +2,9 @@
 
 var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
 
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]; var gMeme = {
+var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]; 
+
+var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
     lines: [ {
