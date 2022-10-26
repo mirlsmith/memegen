df7359bcfae943d3a817f7ece6805470d5c1fd09
Miriam Smith <miriamzeff@gmail.com>
2022-10-26 17:22:24 +0300

    render meme using service

--- js/controllers/memeController.js
+++ js/controllers/memeController.js
@@ -10,30 +10,60 @@ function onInit() {
     // resizeCanvas()
     // window.addEventListener('resize', resizeCanvas)
 
-    renderMeme()
+    const meme = getMeme()
+    renderMeme(meme)
 }
 
-function renderMeme() {
 
-    drawText()
-    drawImg()
+
+
+function renderMeme(meme) {
+    console.log(meme);
+    const {lines, selectedLineIdx:lineIdx , selectedImgId:imgId} = meme
+    console.log(lines);
+    const {align, fillColor, strokeColor, size, txt} = lines[lineIdx]
+    // console.log('font align is:',lines[lineIdx].align);
+    // console.log('font color is:',lines[lineIdx].color);
+    // console.log('font size is:',lines[lineIdx].size);
+    // console.log('txt is:',lines[lineIdx].txt);
+    console.log('font align is:', align);
+    console.log('font stroke color is:', strokeColor);
+    console.log('font fill color is:', fillColor);
+    console.log('font size is:', size);
+    console.log('txt is:', txt);
+    const imgUrl = getImgPath(imgId)
+    console.log('url is:', imgUrl);
+
+    const line = lines[lineIdx]
+
+    drawImgAndText(imgUrl, line)
+
 }
 
-function drawImg() {
+function drawImgAndText(imgUrl, line) {
     const img = new Image() // Create a new html img element
-    img.src = 'img/gallery/1.jpg' // Send a network req to get that image, define the img src
     // When the image ready draw it on the canvas
+    const {align, fillColor, strokeColor, size, font, txt, xPos, yPos} = line
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText(txt, xPos, yPos, size, font, strokeColor, fillColor, align)
     }
+    img.src = imgUrl // Send a network req to get that image, define the img src
 }
 
-function drawText(text = 'Funny Stuff', x=40, y=70) {
+function drawText(text = 'Funny Stuff', x=40, y=70, size, font, strkClr, fillClr, align) {
     gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = 'white'
+    gCtx.strokeStyle = strkClr
+    gCtx.fillStyle = fillClr
 
-    gCtx.font = '40px Impact'
+    gCtx.textAlign = align
+
+    gCtx.font = `${size}px ${font}`
     gCtx.fillText(text, x, y) // Draws (fills) a given text at the given (x, y) position.
     gCtx.strokeText(text, x, y) // Draws (strokes) a given text at the given (x, y) position.
 }
+
+// gCtx.measureText(txt).width
+// מחזיר את אורך השורה שהטקסט דורש
+
+// gCtx.textAlign = ‘center’ גם חשוב
--- js/services/memeService.js
+++ js/services/memeService.js
@@ -2,14 +2,29 @@
 
 var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
 
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]; 
+var gImgs = [{id: 2, url: 'img/gallery/2.jpg', keywords: ['cute', 'dog']}]; 
 
 var gMeme = {
-    selectedImgId: 5,
+    selectedImgId: 2,
     selectedLineIdx: 0,
     lines: [ {
-        txt: 'I sometimes eat Falafel', size: 20,
+        txt: 'Too much Cuteness', 
+        size: 30,
+        font: 'Impact',
         align: 'left',
-        color: 'red'
+        strokeColor: 'red',
+        fillColor: 'purple',
+        xPos: 30,
+        yPos: 50
     } ]
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function getImgPath(imgId) {
+    const img = gImgs.find(image => imgId === image.id)
+    const {url} = img
+    return url
 }
